6179bf9673e100260605a99111d6054b49eede8b
Julian Pawlowski <julian.pawlowski@gmail.com>
2014-10-23 13:51:27 +0200

    typo...

--- helper-client/datacontroller.php
+++ helper-client/datacontroller.php
@@ -2,7 +2,7 @@
     include dirname(__FILE__).'/../includes/dbconnection.php';
     include dirname(__FILE__).'/../includes/getConfiguration.php';
     include dirname(__FILE__).'/../includes/sessionhandler.php';
-    include dirname(__FILE__).'../includes/password_compat/lib/password.php';
+    include dirname(__FILE__).'/../includes/password_compat/lib/password.php';
 
     function saveImage($imagedata)
     {
