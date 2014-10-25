bcf594c9a76b2a6c36a35616efe9a4e946c1dfdd
Julian Pawlowski <julian.pawlowski@gmail.com>
2014-10-25 15:22:50 +0200

    fix availability of update_usersettings.php

--- helper-client/update_usersettings.php
+++ helper-client/update_usersettings.php
@@ -1,6 +1,7 @@
 <?php
 
 include dirname(__FILE__).'/../includes/dbconnection.php';
+include dirname(__FILE__).'/../includes/sessionhandler.php';
 
 if (isset($_GET['uid']) && isset($_GET['backgroundimage'])) {
     $sql = "update usersettings set backgroundimage = '" . $_GET['backgroundimage'] . "' where uid = " . $_GET['uid'];
