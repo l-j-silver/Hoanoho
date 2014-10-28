53554274e9c9f6d0cf4e013f4d9ff5c8763eb126
Julian Pawlowski <julian.pawlowski@gmail.com>
2014-10-28 21:08:22 +0100

    colorize FHEM SVG icon to white

--- mobile/includes/mobile-app.php
+++ mobile/includes/mobile-app.php
@@ -62,6 +62,6 @@ if(("standalone" in window.navigator) && window.navigator.standalone){
 /* redirect to login after session timeout */
 window.setTimeout(function() {
     location.reload(true);
-}, 900000);
+}, 901000);
 <?php } ?>
 </script>
