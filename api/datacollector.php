d1889fd30fa2dd18c78d603e1aa8de313fc733d0
Daniel Schaefer <daniel@schaefer2.de>
2014-12-25 15:09:47 +0100

    wslogger.pl sometime gets weird (unnatural big) values from the weatherstation, added hotfix in api/datacollector.php to catch this wrong values.

--- api/datacollector.php
+++ api/datacollector.php
@@ -7,8 +7,23 @@ if (isset($_POST['json'])) {
     for ($i=0; $i < count($json_decoded->Values); $i++) {
         // insert only if value doesn't already exist
         $result = mysql_query("SELECT ddid from device_data where timestamp_unix = '".$json_decoded->Timestamp."' and valuename = '".str_replace(".", "_", $json_decoded->Values[$i]->Name)."' and deviceident = '".$json_decoded->Name."'");
-        if(mysql_num_rows($result) == 0)
-            mysql_query("INSERT INTO device_data (deviceident, timestamp_unix, timestamp, valuename, value, valueunit, year, month, day) VALUES ('".$json_decoded->Name."', ".$json_decoded->Timestamp.", '".date("Y-m-d H:i:s", $json_decoded->Timestamp)."', '".str_replace(".", "_", $json_decoded->Values[$i]->Name)."', '".$json_decoded->Values[$i]->Value."', '".$json_decoded->Values[$i]->Unit."', ".date('Y', $json_decoded->Timestamp).", ".date('m', $json_decoded->Timestamp).", ".date('d', $json_decoded->Timestamp).")");
+        if(mysql_num_rows($result) == 0) {
+        	$value = $json_decoded->Values[$i]->Value;
+        	
+        	// get last value
+        	$result = mysql_query("SELECT value from device_data where valuename = '".str_replace(".", "_", $json_decoded->Values[$i]->Name)."' and deviceident = '".$json_decoded->Name."' order by ddid desc limit 0,1");
+        	while ($row = mysql_fetch_object($result)) {
+        		if(is_numeric($json_decoded->Values[$i]->Value)) {
+	        		if(abs(($row->value - $value)) > 10000) {
+	        			// if diff to previous value is greater than 10000 then insert previous value (wslogger hotfix)
+	        			$value = $row->value;
+	        		}
+	        	}
+        	}
+
+        	// insert value
+        	mysql_query("INSERT INTO device_data (deviceident, timestamp_unix, timestamp, valuename, value, valueunit, year, month, day) VALUES ('".$json_decoded->Name."', ".$json_decoded->Timestamp.", '".date("Y-m-d H:i:s", $json_decoded->Timestamp)."', '".str_replace(".", "_", $json_decoded->Values[$i]->Name)."', '".$value."', '".$json_decoded->Values[$i]->Unit."', ".date('Y', $json_decoded->Timestamp).", ".date('m', $json_decoded->Timestamp).", ".date('d', $json_decoded->Timestamp).")");
+      	}
     }
 }
 ?>
